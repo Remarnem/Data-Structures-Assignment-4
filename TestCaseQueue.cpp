3c24943927d4badc5156165c6f9a6ab503e0bbf3
Joshua Steege <joshuasteege@gmail.com>
2021-11-01 09:11:59 -0400

    Finished stack/queue test cases, assignment should be done.

--- TestCaseQueue.cpp
+++ TestCaseQueue.cpp
@@ -7,15 +7,19 @@
 #include "Queue.h"
 
 int main() {
+    std::cout << "Creating queue..." << std::endl;
     Queue<int> testQueue;
+    std::cout << "Adding data to the queue..." << std::endl;
     testQueue.enqueue(1);
     testQueue.enqueue(4);
     testQueue.enqueue(2);
+    std::cout << "Printing the queue..." << std::endl;
     testQueue.print();
-
-    std::cout << "Front of queue: " << testQueue.first() << std::endl;
-
+    std::cout << "Testing first()" << std::endl;
+    std::cout << testQueue.first() << std::endl;
+    std::cout << "Testing dequeue()" << std::endl;
     testQueue.dequeue();
-
+    std::cout << "Current queue:" << std::endl;
     testQueue.print();
+    std::cout << "Size of stack: " << testQueue.size() << std::endl;
 }
--- TestCaseStack.cpp
+++ TestCaseStack.cpp
@@ -7,9 +7,19 @@
 #include "Stack.h"
 
 int main() {
+    std::cout << "Creating stack..." << std::endl;
     Stack<int> testStack;
+    std::cout << "Pushing data to the stack..." << std::endl;
     testStack.push(1);
     testStack.push(4);
     testStack.push(2);
+    std::cout << "Printing the stack..." << std::endl;
     testStack.print();
+    std::cout << "Testing top()" << std::endl;
+    std::cout << testStack.top() << std::endl;
+    std::cout << "Testing pop()" << std::endl;
+    testStack.pop();
+    //std::cout << "Current stack:" << std::endl;
+    testStack.print();
+    std::cout << "Size of stack: " << testStack.size() << std::endl;
 }
